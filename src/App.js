f491ff24cedf2f64dae30442a734fb57c0fe3ebf
Gazalrafi <grafi7485>
2022-11-21 20:21:31 +0530

    loding commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import './App.css';
 
 function App() {
   const [movies,setMovies] =useState([]);
+  const [isLoading,setIsLoading]=useState(false);
 
  async function fetchMoviesHandler(){
+
+ setIsLoading(true);
+
    const response= await fetch('https://swapi.dev/api/films/');
    const data=await response.json();  
 
@@ -18,7 +22,9 @@ function App() {
           releaseDate:movieData.release_date,
         };
       });
+      
      setMovies(transformedMovies);
+     setIsLoading(false);
   }
 
   return (
@@ -27,10 +33,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       {!isLoading &&  movies.length>0 && <MoviesList movies={movies}/>}
+       {!isLoading && movies.length===0 && <p>Found no movies.</p>}
+       {isLoading && <p>page is loading...</p>}
       </section>
     </React.Fragment>
   );
 }
-
 export default App;
